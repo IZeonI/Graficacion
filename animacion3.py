cc18c115feea9e46e79eb21f82567603a5746d2e
IZeonI <osvaldomartinezespinosa@gmail.com>
2024-09-24 16:56:28 -0600

    Mejora a la animacion en ciclo

--- animacion3.py
+++ animacion3.py
@@ -4,19 +4,19 @@ import cv2
 width, height= 1000,1000
 img=np.ones((height,width,3), dtype=np.uint8)*255
 
-a,b,c,d,j,k=1,4,4,1,3,3
+a,b,c,d,j,k=1,5,1,5,3,3
 theta_increment=0.05
 max_theta=2*np.pi
 
-center_x, center_y = width//2, height//2
+center_x, center_y = (width//2), height//2
 theta=0
 
 while True:
     img=np.ones((width,height,3), dtype=np.uint8)*255
     
     for t in np.arange(0,theta,theta_increment):
-        x = int(center_x+200*np.cos(a * t) - (np.cos(b * t)) ** j)
-        y = int(center_y+200*np.sin(c * t) - (np.sin(d * t)) ** k)
+        x = int((center_x+400*np.cos(a * t) - center_x*(np.cos(b * t)) ** j)/2)+200
+        y = int((center_y+400*np.sin(c * t) - center_y*(np.sin(d * t)) ** k)/2)+100
         
         cv2.circle(img,(x,y),1,(0,0,255),2)
         
